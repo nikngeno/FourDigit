24125ed833f300950c15d69a457cd2383e207624
Nick <nikngeno@gmail.com>
2024-09-07 14:50:30 -0700

    Update the code with the four extra questions

--- Program.cs
+++ Program.cs
@@ -21,7 +21,19 @@ namespace FourDigit
             Console.WriteLine(fourDig);
             
 
+            Console.WriteLine(fourDigit);
             Console.WriteLine("The Sum of the digits is: " + (firstDig + secondDig + thirdDig + fourDig));
+
+            int reverseOrder = (firstDig *1000) + (secondDig *100) + (thirdDig *10) + fourDig;
+            Console.WriteLine(reverseOrder);
+            int lastToFirst = (firstDig *1000) + (thirdDig *100) + (secondDig *10) + fourDig;
+            Console.WriteLine(lastToFirst);
+            int secondToThird = (fourDig * 1000) + (secondDig* 100) + (thirdDig * 10) + firstDig;
+            Console.WriteLine(secondToThird);
+
+
+        }
+            
         }
     }
-}
+
